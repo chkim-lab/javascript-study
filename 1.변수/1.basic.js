8d68ccfb92b2b7d4d8d4f6e194960fe8e43d1c25
chkim <oraedu03@naver.com>
2021-01-05 11:03:43 +0900

    first javascript

---
+++ 1.변수/1.basic.js
@@ -0,0 +1,21 @@
+
+
+//변수 선언
+var total;
+console.log(total);
+
+total = 100;
+console.log(total);
+/* 변수의선언과 초기화를 동시에 */
+var age = 27;
+console.log(age);
+
+total = 50;
+console.log(total);
+
+var copyAge = age;
+console.log(copyAge);
+
+var x = 10;
+x = x + 3;
+console.log(x);
